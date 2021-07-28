cfdba2718f40c32b3a01916003a33e9dfab04470
FraustoB <brandon.frausto18@gmail.com>
2021-07-27 21:24:21 -0500

    added navbar, added responsive navbar, added navbar toggle commands

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
 
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
 export default MyApp
