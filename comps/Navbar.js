83e2bf7e1ee7f59bfe1818d5d0e747361bebf617
FraustoB <brandon.frausto18@gmail.com>
2021-07-29 21:56:34 -0500

    added menu button for mobile in hops of fixing phone rendering issues

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -17,9 +17,7 @@ export default function Navbar() {
             <div className={styles.Navbar}>
                 <Link href='/'><a style={{ textDecoration: 'none' }}> <div className={styles.Brand}>{'<'}frausto.dev{'>'}</div> </a></Link>
                 <button onClick={handleIsToggled}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                    <span>Menu</span>
                 </button>
 
 
